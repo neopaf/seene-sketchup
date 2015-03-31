d325973fa56f3666bd9d21a02a5ae6a04a1a3572
Alexander Petrossian (PAF@home) <alexander.petrossian@teligent.ru>
2015-04-01 02:07:59 +0300

    initial

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # seene-sketchup
 Importer plugin for Sketchup, loads scene.oemodel and texturizes it with poster.jpg  (see seene-backup to get them)
+
+# install
+
+Install Sketchup from http://www.sketchup.com
+
+Put [seene.rb] file to your Sketchup plugins directory
+* Mac: /Users/<your user name>/Library/Application Support/SketchUp 2015/SketchUp/Plugins
+* Windows: C:\Program Files (x86)\SketchUp\SketchUp 2015\Plugins
+
+# use
+
+0. Download some Seenes using https://github.com/neopaf/seene-backup (or use sample files below)
+1. Start Sketchup
+2. File|New
+3. File|Import...
+4. Select 'Seene' as import type
+5. Pick scene.oemodel you want to import (poster.jpg should be in same folder with it)
+6. Be happy
+
+# known problems
+
+Does NOT work well with new models of 240x240 scene.oemodel; working on it...
+
+# sample files
+
+[samples/paf-first-staffpick/poster.jpg]
+[samples/paf-first-staffpick/scene.oemodel]
---
+++ seene.rb
@@ -0,0 +1,217 @@
+#
+# Importer plugin for Sketchup, loads scene.oemodel and texturizes it with poster.jpg (
+# see https://github.com/neopaf/seene-backup to get them)
+#
+# Copyright 2014 Alexander Petrossian (PAF) alexander.petrossian+seene.rb@gmail.com
+#
+# https://github.com/neopaf/seene-backup
+#
+
+
+
+# Copyright 2004-2005, Todd Burch - Burchwood USA   http://www.burchwoodusa.com
+
+# THIS SOFTWARE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
+# Name :          progressbar.rb 1.0
+# Description :   Creates a text-based progress bar on the status line.
+# Author :        Todd Burch   http://www.burchwoodusa.com 
+# Usage :         1. to create a progress bar from your script: 
+#
+#                 pb = ProgressBar.new(total_items_to_process{,optional_process_name}) 
+#
+#                 To update the status bar: 
+#
+#                 pb.update(current_item_number_being_processed)  
+#
+#                
+# Date :          11.Nov.2005
+# Type :          Module 
+# History:        1.0 (11.Nov.2005) - first version
+#
+#
+#-----------------------------------------------------------------------------
+
+class ProgressBar 
+
+@@err_total_notnumeric = "ProgressBar: Total must be a positive integer."
+@@err_count_notnumeric = "ProgressBar: Iteration Count must be numeric"
+@@end_time             = "Expected End Time:" 
+@@progresschar = ">" ; 
+@@initial_block = "-" * 50     # Default progress bar line sequence.
+
+def initialize(total,phase=nil)  
+  if (!total.integer? or total < 0)
+    raise(ArgumentError,@@err_total_notnumeric) 
+    return ; 
+    end ; 
+  @total = total.to_i ; 
+  @phase = phase ; 
+  @firsttime = true ; 
+  end ; 
+
+def update(iteration) 
+  if !iteration.integer? 
+    raise(ArgumentError,@@err_count_notnumeric)  
+    return ; 
+    end ; 
+  iteration = [iteration.abs,@total].min  # make sure we don't exceed the total count or have a value < 0
+  pct = [1,(iteration*100)/@total].max    # Calculate percentage complete.
+                                          # round up to 1% if anything less than 1%.
+  end_time = "?" ; 
+  if @firsttime then ; 
+    # Get the current time of day. 
+    # set up an elapsed timer so we can calculate expected end time.  
+    @time1 = Time.now ;     # Get current time of day. 
+    @firsttime = false ;    # turn off switch 
+  else 
+    # divide the elapsed time by the pct complete, then multiple that by 100, then add that to the 
+    # start time, and that is the expected end time.  
+    end_time = Time.at(((((Time.now-@time1).to_f)/pct)*100 + @time1.to_f).to_i).strftime("%c") 
+    end ; 
+  pct_pos = [pct/2,1].max ;
+  current_block = @@initial_block[0,pct_pos-1] << @@progresschar << @@initial_block[pct_pos,@@initial_block.length]
+  Sketchup.set_status_text(current_block << "   " << (pct.to_s)<<"%. #{@@end_time} " << end_time <<" #{@phase}")
+  end ; 
+
+end ; # class ProgressBar ; 
+
+class SeeneImporter < Sketchup::Importer
+
+       # This method is called by SketchUp to determine the description that
+       # appears in the File > Import dialog's pulldown list of valid
+       # importers.
+       def description
+         return "Seene (*.oemodel)"
+       end
+
+       # This method is called by SketchUp to determine what file extension
+       # is associated with your importer.
+       def file_extension
+         return "oemodel"
+       end
+
+       # This method is called by SketchUp to get a unique importer id.
+       def id
+         return "com.sketchup.importers.seene"
+       end
+
+       # This method is called by SketchUp to determine if the "Options"
+       # button inside the File > Import dialog should be enabled while your
+       # importer is selected.
+       def supports_options?
+         return false #true
+       end
+
+       # This method is called by SketchUp when the user clicks on the
+       # "Options" button inside the File > Import dialog. You can use it to
+       # gather and store settings for your importer.
+       def do_options
+         # In a real use you would probably store this information in an
+         # instance variable.
+         my_settings = UI.inputbox(['My Import Option:'], ['1'],
+           "Import Options")
+       end
+
+       # This method is called by SketchUp after the user has selected a file
+       # to import. This is where you do the real work of opening and
+       # processing the file.
+       def load_file(file_path, status)
+folder = File.dirname(file_path)
+
+buffer = File.binread(file_path)
+version,
+camera_width,
+camera_height,
+camera_fx,
+camera_fy,
+camera_k1,
+camera_k2,
+depthmap_width,
+depthmap_height = buffer.unpack("LLLffffLL")
+header_size = 36
+
+body = buffer[header_size, buffer.length]
+depthmap = body.unpack("f*")
+
+#puts depthmap_width
+
+def v(x,y,depthmap,depthmap_width,depthmap_height)
+	depth = depthmap[y * depthmap_width + x]
+	return Geom::Point3d.new(
+		x,
+		y,
+		-depth * depthmap_width)
+end
+
+pb = ProgressBar.new(depthmap_height-2, "Making faces...")
+mesh = Geom::PolygonMesh.new
+for y in 0..depthmap_height-2
+for x in 0..depthmap_width-2
+	mesh.add_polygon(
+		v(x+0, y+0, depthmap,depthmap_width,depthmap_height),
+		v(x+1, y+0, depthmap,depthmap_width,depthmap_height),
+		v(x+1, y+1, depthmap,depthmap_width,depthmap_height),
+	)
+	mesh.add_polygon(
+		v(x+0, y+0, depthmap,depthmap_width,depthmap_height),
+		v(x+1, y+1, depthmap,depthmap_width,depthmap_height),
+		v(x+0, y+1, depthmap,depthmap_width,depthmap_height)
+	)
+end
+pb.update(y)
+end
+
+
+model = Sketchup.active_model
+group = model.entities.add_group
+
+materials = model.materials
+b_material = materials.add('Back')
+b_material.texture = 'black'
+f_material = materials.add('Front')
+f_material.texture = File.expand_path("poster.jpg",folder)
+
+#first_point = v(0, 0, depthmap,depthmap_width,depthmap_height)
+last_point = v(depthmap_width-1, depthmap_height-1, depthmap,depthmap_width,depthmap_height)
+#f_material.texture.size = last_point.x - first_point.x
+f_material.texture.size = last_point.x
+smooth_flags = Geom::PolygonMesh::AUTO_SOFTEN
+
+pb = ProgressBar.new(1, "Adding faces (slow)")
+group.entities.add_faces_from_mesh mesh, smooth_flags, f_material, b_material
+
+pb = ProgressBar.new(1, "Setting projection properties")
+vector_down = Geom::Vector3d.new 0,0,1
+group.entities.each { |entity|
+	if entity.is_a? Sketchup::Face
+		entity.set_texture_projection(vector_down,true)
+	end
+}
+
+pb = ProgressBar.new(1, "Moving camera")
+
+# Create a camera from scratch with an "eye" position in
+# x, y, z coordinates, a "target" position that
+# defines what to look at, and an "up" vector.
+
+eye = [last_point.x/2,last_point.y/2,depthmap_width]
+target = [last_point.x/2,last_point.y/2,0]
+up = [-1,0,0]
+my_camera = Sketchup::Camera.new eye, target, up
+
+# Get a handle to the current view and change its camera.
+view = Sketchup.active_model.active_view
+view.camera = my_camera
+
+
+pb = ProgressBar.new(1, "Done")
+
+         return 0 # 0 is the code for a successful import
+       end
+     end
+
+     Sketchup.register_importer(SeeneImporter.new)
+#UI.messagebox("hi")
