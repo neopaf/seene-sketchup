a8413817411f00283b710fe7fb321775f380c535
Alexander Petrossian (PAF@home) <alexander.petrossian@teligent.ru>
2015-04-06 17:22:34 +0300

    tried to switch to perspective camera, as in web-player, very weird code with twisted geometry, abandoning that attempt

--- seene.rb
+++ seene.rb
@@ -80,6 +80,10 @@ camera_k1,
 camera_k2,
 depthmap_width,
 depthmap_height = buffer.unpack("LLLffffLL")
+header_bytes = 36
+if version != 2 then
+	header_bytes= 44
+end
 
 #@jmia claimed this did not work for her# raise "Importer works with versions 2 and 3 only, and this is version " + version.to_s unless version==3 || version==2
 #3
@@ -92,19 +96,34 @@ depthmap_height = buffer.unpack("LLLffffLL")
 #240
 #240
 
-header_size = 36
-
-body = buffer[header_size, buffer.length]
+body = buffer[header_bytes, buffer.length]
 depthmap = body.unpack("f*")
 
 #puts depthmap_width
 
-def v(x,y,depthmap,depthmap_width,depthmap_height)
+=begin
+                    // TODO: Not sure what these are. Some sort of coefficients.
+                    var xk = header.camera_fx / header.camera_width;
+                    var yk = header.camera_fy / header.camera_height;
+
+                    for (var y = 0; y < header.depthmap_height; ++y) {
+                        for (var x = 0; x < header.depthmap_width; ++x) {
+                            var depth = depthmap[y * header.depthmap_width + x];
+                            geometry.vertices.push(
+                                new THREE.Vector3(
+                                    depth * ((x + 0.5) / header.depthmap_width - 0.5) / xk,
+                                    -depth * ((y + 0.5) / header.depthmap_height - 0.5) / yk,
+                                    -(depth - 1)
+=end
+
+xk = camera_fx / camera_width
+yk = camera_fy / camera_height
+def v(x,y,depthmap,depthmap_width,depthmap_height,xk,yk=)
 	depth = depthmap[y * depthmap_width + x]
 	return Geom::Point3d.new(
-		x,
-		-y,
-		-depth * depthmap_width)
+		depth * ((x+0.5) / depthmap_width -0.5) / xk
+		-depth * ((y+0.5) / depthmap_height -0.5) / yk
+		-(depth - 1))
 end
 
 mesh = Geom::PolygonMesh.new
@@ -114,15 +133,16 @@ y = 0
 while y < depthmap_height-block_size
 x = 0
 while x < depthmap_width-block_size
+#puts v(x+0, y+0, depthmap,depthmap_width,depthmap_height,xk,yk).to_s
 	mesh.add_polygon(
-		v(x+0, y+0, depthmap,depthmap_width,depthmap_height),
-		v(x+block_size, y+0, depthmap,depthmap_width,depthmap_height),
-		v(x+block_size, y+block_size, depthmap,depthmap_width,depthmap_height),
+		v(x+0, y+0, depthmap,depthmap_width,depthmap_height,xk,yk),
+		v(x+block_size, y+0, depthmap,depthmap_width,depthmap_height,xk,yk),
+		v(x+block_size, y+block_size, depthmap,depthmap_width,depthmap_height,xk,yk),
 	)
 	mesh.add_polygon(
-		v(x+0, y+0, depthmap,depthmap_width,depthmap_height),
-		v(x+block_size, y+block_size, depthmap,depthmap_width,depthmap_height),
-		v(x+0, y+block_size, depthmap,depthmap_width,depthmap_height)
+		v(x+0, y+0, depthmap,depthmap_width,depthmap_height,xk,yk),
+		v(x+block_size, y+block_size, depthmap,depthmap_width,depthmap_height,xk,yk,),
+		v(x+0, y+block_size, depthmap,depthmap_width,depthmap_height,xk,yk)
 	)
 	x = x + block_size
 end
@@ -136,14 +156,12 @@ group = model.entities.add_group
 materials = model.materials
 f_material = materials.add('Front')
 f_material.texture = File.expand_path("poster.jpg",folder)
+#f_material.texture = 'green'
 #b_material = materials.add('Back')
 #b_material.texture = 'black'
 b_material = f_material
 
-#first_point = v(0, 0, depthmap,depthmap_width,depthmap_height)
-last_point = v(depthmap_width-block_size, depthmap_height-block_size, depthmap,depthmap_width,depthmap_height)
-#f_material.texture.size = last_point.x - first_point.x
-f_material.texture.size = last_point.x
+#f_material.texture.size = 1/xk
 smooth_flags = Geom::PolygonMesh::AUTO_SOFTEN
 #smooth_flags = Geom::PolygonMesh::AUTO_SOFTEN + Geom::PolygonMesh::SMOOTH_SOFT_EDGES
 
@@ -169,14 +187,30 @@ group.entities.each { |entity|
 
 ## Moving camera
 
+=begin
+                    var camera = new THREE.PerspectiveCamera(
+                        Math.atan2(0.495 * header.camera_width, header.camera_fx) * 360 / Math.PI,
+                        this.width / this.height,
+                        .01,
+                        100
+                    );
+                    camera.position.set(0, 0, 1);
+                    camera.up.set(-1, 0, 0);
+=end
+
 # Create a camera from scratch with an "eye" position in
 # x, y, z coordinates, a "target" position that
 # defines what to look at, and an "up" vector.
-
-eye = [last_point.x/2,last_point.y/2,depthmap_width*2]
-target = [last_point.x/2,last_point.y/2,0]
-up = [-1,0,0]
-my_camera = Sketchup::Camera.new eye, target, up, false
+##eye = [last_point.x/2,last_point.y/2,depthmap_width*2]
+##target = [last_point.x/2,last_point.y/2,0]
+##up = [-1,0,0]
+##my_camera = Sketchup::Camera.new eye, target, up, false
+my_camera = Sketchup::Camera.new(
+	[camera_width/2,camera_height/2,1], #eye
+	[camera_width/2,camera_height/2,0], #target
+	[-1,0,0], #up
+	true,  #perspective
+	Math.atan2(0.495 * camera_width, camera_fx) * 360 / Math::PI) #fov
 
 # Get a handle to the current view and change its camera.
 view = Sketchup.active_model.active_view
@@ -254,7 +288,7 @@ end
 
 folder = "/tmp" # @TODO
 
-version =	3
+version =	2
 camera_width = 1936 # (jpg
 camera_height = 1936 # sizes)
 camera_fx = 2334.201416015625 #fx?
